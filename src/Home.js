0bd406abb5460d0b8291afc4019e1e12a34273f8
lindayang <linda.yang@formlabs.com>
2019-03-13 16:39:38 -0400

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+            Hello, this is fluffball's initial react project.
+          </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
