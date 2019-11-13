e81314be3d432de4aab32ee49d6accaf57758fd5
lindaayangg <lindayang0928@hotmail.com>
2019-11-12 19:43:53 -0600

    Initial design of landing page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react";
-import Footer from "../../components/Footer/Footer";
+import {GridColumn, Image} from "semantic-ui-react";
 import {StyledBody} from "../../res/styles";
+import {
+  StyledGridLeftColumn,
+  StyledGridWrapper,
+  StyledLandingImage,
+  StyledSubTitleText,
+  StyledTitleText
+} from "./styles";
 
 class Home extends Component {
   render() {
     return (
       <>
         <StyledBody>
+          <StyledGridWrapper columns={2}>
+            <StyledGridLeftColumn width={7} verticalAlign='middle'>
+              <StyledTitleText>Placeholder Text</StyledTitleText>
+              <StyledSubTitleText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </StyledSubTitleText>
+            </StyledGridLeftColumn>
+            <GridColumn width={9}>
+              <StyledLandingImage src='https://polymagnus.com/svg/services/webDesign.svg'/>
+            </GridColumn>
+          </StyledGridWrapper>
         </StyledBody>
       </>
     )
