a97d52dd47a48b770b8219d1aaf1775a89387ed5
lindaayangg <lindayang0928@hotmail.com>
2019-11-19 20:11:36 -0600

    Add information and design to About Screen

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,11 +1,261 @@
 import React, {Component} from 'react';
+import {StyledComponentWrapper} from "../../res/styles";
+import {
+  StyledAboutText,
+  StyledAboutTextWrapper,
+  StyledAboutWrapper,
+  StyledArrows,
+  StyledCarouselWrapper,
+  StyledHackathonDescription,
+  StyledHackathonDescriptionWrapper,
+  StyledHackathonGrid,
+  StyledHackathonTitle,
+  StyledHackthonCardWrapper,
+  StyledMemberDetailLeftWrapper,
+  StyledMemberDetailRightWrapper,
+  StyledMemberInfo,
+  StyledMemberName,
+  StyledMemberTitle,
+  StyledMemberWrapper,
+  StyledSectionWrapper,
+  StyledSocialMediaButton,
+  StyledSubTitleText,
+  StyledTeamWrapper,
+  StyledTitleText
+} from "./styles";
+import strings from "../../res/strings";
+import {Card, GridColumn, Icon, Image} from "semantic-ui-react";
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import hackthe6ix from '../../res/images/hackthe6ix.jpg';
+import hackthe6ix_stage from '../../res/images/hackthe6ix_stage.jpg';
+import hackthe6ix_stage1 from '../../res/images/hackthe6ix_stage1.jpg';
 
 class About extends Component {
-  render() {
-    return(
+
+  renderAboutUs = () => {
+    return (
       <div>
-        This is the about page.
+        <StyledTitleText>
+          {strings.aboutScreen.aboutUsTitle}
+        </StyledTitleText>
+        <StyledSubTitleText>
+          {strings.aboutScreen.subtitle}
+        </StyledSubTitleText>
+        <StyledAboutTextWrapper>
+          <StyledAboutText>
+            {strings.aboutScreen.aboutText}
+          </StyledAboutText>
+        </StyledAboutTextWrapper>
       </div>
+    )
+  };
+
+  renderCarousel = () => {
+    const carouselSettings = {
+      infinite: true,
+      animationSpeed: 1000,
+      slidesPerPage: 1,
+      arrowLeft: <StyledArrows name="angle double left" size='large'/>,
+      arrowRight: <StyledArrows name="angle double right" size='large'/>,
+      addArrowClickHandler: true,
+    };
+    return (
+      <Carousel {...carouselSettings}>
+        <StyledHackthonCardWrapper>
+          <Card>
+            <Image src={hackthe6ix} size='medium'/>
+            <Card.Content>
+              <Card.Description>
+                {strings.aboutScreen.inspiration.hackathon.pictureDescription1}
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </StyledHackthonCardWrapper>
+        <StyledHackthonCardWrapper>
+          <Card>
+            <Image src={hackthe6ix_stage1} size='medium'/>
+            <Card.Content>
+              <Card.Description>
+                {strings.aboutScreen.inspiration.hackathon.pictureDescription2}
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </StyledHackthonCardWrapper>
+        <StyledHackthonCardWrapper>
+          <Card>
+            <Image src={hackthe6ix_stage} size='medium'/>
+            <Card.Content>
+              <Card.Description>
+                {strings.aboutScreen.inspiration.hackathon.pictureDescription2}
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </StyledHackthonCardWrapper>
+      </Carousel>
+    )
+  };
+
+  renderInspiration = () => {
+    return (
+      <StyledSectionWrapper>
+        <StyledTitleText>
+          {strings.aboutScreen.inspiration.title}
+        </StyledTitleText>
+        <StyledCarouselWrapper>
+          <StyledHackathonGrid columns={2}>
+            <GridColumn width={7}>
+              {this.renderCarousel()}
+            </GridColumn>
+            <StyledHackathonDescriptionWrapper width={9}>
+              <StyledHackathonTitle>
+                {strings.aboutScreen.inspiration.hackathon.title}
+              </StyledHackathonTitle>
+              <StyledHackathonDescription>
+                {strings.aboutScreen.inspiration.hackathon.description}
+              </StyledHackathonDescription>
+            </StyledHackathonDescriptionWrapper>
+          </StyledHackathonGrid>
+        </StyledCarouselWrapper>
+      </StyledSectionWrapper>
+    )
+  };
+
+  renderMeetTeam = () => {
+    return (
+      <StyledSectionWrapper>
+        <StyledTitleText>
+          {strings.aboutScreen.meetTeam.title}
+        </StyledTitleText>
+        <StyledTeamWrapper>
+          <StyledMemberWrapper columns={2}>
+            <GridColumn width={5}>
+              <Image src='https://devlinda.com/assets/img/profile.png' rounded/>
+            </GridColumn>
+            <StyledMemberDetailLeftWrapper width={11}>
+              <StyledMemberName>
+                {strings.aboutScreen.meetTeam.david.name}
+              </StyledMemberName>
+              <StyledMemberTitle>
+                {strings.aboutScreen.meetTeam.david.title}
+              </StyledMemberTitle>
+              <StyledMemberInfo>
+                {strings.aboutScreen.meetTeam.david.detail}
+              </StyledMemberInfo>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://www.linkedin.com/in/davidhqr/'
+                target='_blank'
+              >
+                <Icon name='linkedin'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://github.com/davidhqr'
+                target='_blank'
+              >
+                <Icon name='github alternate'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://devdavid.com/'
+                target='_blank'
+              >
+                <Icon name='linkify'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='mailto:shiwei.zhang@uwaterloo.ca'
+                target='_blank'
+              >
+                <Icon name='mail'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://www.facebook.com/davidhqr'
+                target='_blank'
+              >
+                <Icon name='facebook f'/>
+              </StyledSocialMediaButton>
+            </StyledMemberDetailLeftWrapper>
+          </StyledMemberWrapper>
+        </StyledTeamWrapper>
+        <StyledTeamWrapper>
+          <StyledMemberWrapper columns={2}>
+            <StyledMemberDetailRightWrapper width={11}>
+              <StyledMemberName>
+                {strings.aboutScreen.meetTeam.linda.name}
+              </StyledMemberName>
+              <StyledMemberTitle>
+                {strings.aboutScreen.meetTeam.linda.title}
+              </StyledMemberTitle>
+              <StyledMemberInfo>
+                {strings.aboutScreen.meetTeam.linda.detail}
+              </StyledMemberInfo>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://www.linkedin.com/in/lindaayangg/'
+                target='_blank'
+              >
+                <Icon name='linkedin'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://github.com/lindaayangg'
+                target='_blank'
+              >
+                <Icon name='github alternate'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://devlinda.com/'
+                target='_blank'
+              >
+                <Icon name='linkify'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='mailto:xiaoling.yang@uwaterloo.ca'
+                target='_blank'
+              >
+                <Icon name='mail'/>
+              </StyledSocialMediaButton>
+              <StyledSocialMediaButton
+                icon
+                circular
+                href='https://www.facebook.com/lindayanggg'
+                target='_blank'
+              >
+                <Icon name='facebook f'/>
+              </StyledSocialMediaButton>
+            </StyledMemberDetailRightWrapper>
+            <GridColumn width={5}>
+              <Image src='https://devlinda.com/assets/img/profile.png' rounded/>
+            </GridColumn>
+          </StyledMemberWrapper>
+        </StyledTeamWrapper>
+      </StyledSectionWrapper>
+    )
+  };
+
+  render() {
+    return (
+      <StyledComponentWrapper>
+        <StyledAboutWrapper>
+          {this.renderAboutUs()}
+          {this.renderMeetTeam()}
+          {this.renderInspiration()}
+        </StyledAboutWrapper>
+      </StyledComponentWrapper>
     )
   }
 }
---
+++ src/screens/About/styles.js
@@ -0,0 +1,167 @@
+import styled from "styled-components";
+import {Button, Grid, GridColumn, Header, Icon} from "semantic-ui-react";
+import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
+
+export const StyledAboutWrapper = styled.div`
+  &&& {
+    padding: 60px !important;
+    text-align: center;
+    width: 100%;
+  }
+`;
+
+export const StyledTitleText = styled(Header)`
+  &&& {
+    color: #FA7268;
+    font-size: 35px;
+  }
+`;
+
+export const StyledSectionWrapper = styled.div`
+  &&& {
+    margin-top: 100px;
+  }
+`;
+
+export const StyledSubTitleText = styled(HeaderSubHeader)`
+  &&& {
+    font-size: 18px;
+    color: #676767;
+  }
+`;
+
+export const StyledAboutTextWrapper = styled.div`
+  &&& {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 15px;
+    color: #898989;
+  }
+`;
+
+export const StyledAboutText = styled.p`
+  &&& {
+    width: 55%;
+    line-height: 1.6rem;
+  }
+`;
+
+export const StyledTeamWrapper = styled.div`
+  &&& {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const StyledMemberWrapper = styled(Grid)`
+  &&& {
+    width: 70%;
+  }
+`;
+
+export const StyledMemberName = styled(Header)`
+  &&& {
+    font-size: 23px;
+    margin-bottom: 8px;
+  }
+`;
+
+export const StyledMemberTitle = styled(HeaderSubHeader)`
+  &&& {
+    font-size: 17px;
+    color: #FA7268;
+    font-weight: 600;
+  }
+`;
+
+export const StyledMemberInfo = styled.p`
+  &&& {
+    margin-top: 20px;
+    line-height: 1.8rem;
+    font-size: 15px;
+  }
+`;
+
+export const StyledMemberDetailLeftWrapper = styled(GridColumn)`
+  &&& {
+    text-align: left;
+    padding-left: 70px !important;
+  }
+`;
+
+export const StyledMemberDetailRightWrapper = styled(GridColumn)`
+  &&& {
+    text-align: left;
+    padding-right: 70px !important;
+  }
+`;
+
+export const StyledSocialMediaButton = styled(Button)`
+  &&& {
+    background-color: #FA7268;
+    color: #ffffff;
+    margin-right: 10px;
+    &:hover{
+      background-color: #FA7268;
+      color: #ffffff;
+      box-shadow: 0 3px 3px 0 rgba(0,0,0,.24), 0 0 2px 0 rgba(0,0,0,.12);
+    }
+    &:focus{
+      background-color: #FA7268;
+      color: #ffffff;
+    }
+  }
+`;
+
+export const StyledCarouselWrapper = styled.div`
+  &&& {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+  }
+`;
+
+export const StyledHackathonGrid = styled(Grid)`
+  &&& {
+    width: 75%;
+  }
+`;
+
+export const StyledHackathonDescriptionWrapper = styled(GridColumn)`
+  &&& {
+    padding-left: 50px !important;
+    text-align: left;
+  }
+`;
+
+export const StyledArrows = styled(Icon)`
+  &&& {
+    color: #FA7268;
+  }
+`;
+
+export const StyledHackathonTitle = styled(Header)`
+  &&& {
+    font-size: 20px;
+    color: #FA7268;
+  }
+`;
+
+export const StyledHackathonDescription = styled.p`
+  &&& {
+    margin-top: 20px;
+    line-height: 1.8rem;
+    font-size: 15px;
+  }
+`;
+
+export const StyledHackthonCardWrapper = styled.div`
+  &&& {
+    padding: 10px 0;
+  }
+`;
